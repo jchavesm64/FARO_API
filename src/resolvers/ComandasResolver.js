dc9db59974ef5c0d57978bf817cc332e85783a4e
Maito1794 <isaacmmorab@gmail.com>
2024-10-18 10:30:02 -0600

    Fix obtenerComandas query

--- src/resolvers/ComandasResolver.js
+++ src/resolvers/ComandasResolver.js
@@ -4,14 +4,77 @@ export default {
     Query: {
         obtenerComandas: async (_, { }) => {
             try {
-                const comandas = await Comanda.find({}).populate('mesa');
-                for (const comanda of comandas) {
-                    const subcuentas = await Subcuenta.find({ comanda: comanda.id });
-                    comanda.subcuentas = subcuentas;
-                }
-                return comandas;
+                const comandas = await Comanda.aggregate([
+                    {
+                        $match: {
+                            estado: 'GENERADA'
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: 'subcuentas',
+                            localField: '_id',
+                            foreignField: 'comanda',
+                            as: 'subcuentas'
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: 'mesas',
+                            localField: 'mesa',
+                            foreignField: '_id',
+                            as: 'mesa'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$mesa',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: 'pisos',
+                            localField: 'mesa.piso',
+                            foreignField: '_id',
+                            as: 'mesa.piso'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$mesa.piso',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    }
+                ]);
+
+                const formattedComandas = comandas.map(comanda => ({
+                    id: comanda._id.toString(),
+                    fecha: comanda.fecha,
+                    observaciones: comanda.observaciones,
+                    mesa: {
+                        id: comanda.mesa._id.toString(),
+                        numero: comanda.mesa.numero,
+                        piso: {
+                            id: comanda.mesa.piso._id.toString(),
+                            nombre: comanda.mesa.piso.nombre,
+                        },
+                        tipo: comanda.mesa.tipo,
+                    },
+                    subcuentas: comanda.subcuentas.map(subcuenta => ({
+                        id: subcuenta._id.toString(),
+                        platillos: subcuenta.platillos.map(platillo => ({
+                            id: platillo.id.toString(),
+                            cantidad: platillo.cantidad,
+                            nombre: platillo.nombre,
+                            entregados: platillo.entregados,
+                        }))
+                    }))
+                }));
+                return formattedComandas;
             } catch (error) {
-                return error;
+                console.error(error);
+                throw new Error('Error fetching comandas');
             }
         },
         obtenerComandaById: async (_, { id }) => {
@@ -104,7 +167,6 @@ export default {
         desactivarComanda: async (_, { id }) => {
             try {
                 const comanda = await Comanda.findOneAndUpdate({ _id: id }, { estado: 'DESCARTADA' }, { new: true });
-                console.log(id);
                 if (!comanda) {
                     return {
                         estado: false,
